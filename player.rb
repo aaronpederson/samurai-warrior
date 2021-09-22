f3dde3e238bdce2baf9506004eb491ce64da1348
aaronpederson <aaron.pederson@gmail.com>
2021-09-22 15:43:51 -0700

    feat(Level 5): Initial Commit.

--- player.rb
+++ player.rb
@@ -1,8 +1,12 @@
+# require "pry"
+# binding.pry  
+
 class Player
   FULL_HEALTH = 20
   def play_turn(warrior)
-    if warrior.feel.empty?
-      if warrior.health < FULL_HEALTH
+    @last_health ||= warrior.health
+    if warrior.feel.empty? 
+      if warrior.health < FULL_HEALTH && (warrior.health >= @last_health)
         warrior.rest!
       else
         warrior.walk!
@@ -10,5 +14,6 @@ class Player
     else
       warrior.attack!
     end
+    @last_health = warrior.health
   end
 end
